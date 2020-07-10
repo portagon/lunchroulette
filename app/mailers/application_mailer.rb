e29237260d1781d263ea45a96922576b72ee798c
Kevin Liebholz <liebholz@crowddesk.de>
2020-07-10 16:46:18 +0200

    WIP: test method for mailer in production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'admin@crowddesk.de'
   layout 'mailer'
+
+  def test
+    mail(to: 'liebholz@crowddesk.de', subject: 'It works', body: 'Yeah it does dude!')
+  end
 end
