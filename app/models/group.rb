c8f166e561b8876cde685755e4914eaeaac4140d
Kevin Liebholz <liebholz@crowddesk.de>
2020-07-14 17:26:25 +0200

    FEAT: #4 Groups get assigned for multiples of PERSONS_PER_GROUP inkl. test

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,42 @@
 class Group < ApplicationRecord
-  belongs_to :leader, class_name: 'User'
-  has_many :lunches
+  PERSONS_PER_GROUP = 4
 
-  validates :leader, :restaurant, :date, presence: true
+  belongs_to :leader, class_name: 'User', optional: true
+  has_many :lunches
+  has_many :users, through: :lunches
+
+  validates :date, presence: true
+
+  def self.create_all_groups!
+    date = Date.today
+    lunches = Lunch.on(date)
+    groups_count = lunches.count / PERSONS_PER_GROUP
+    groups = (1..groups_count).map { Group.new(date: date) }
+    assign_group_sizes(lunches, groups)
+  end
+
+  def create_group!(remaining_lunches)
+    save
+    assign_lunches(remaining_lunches)
+    update(leader: lunches.sample.user)
+  end
+
+  private
+
+  def assign_lunches(lunches)
+    lunches.sample(4).map do |lunch|
+      lunch.group = self
+      lunch.save
+    end
+  end
+
+  def self.assign_group_sizes(lunches, groups)
+    # groups_with_5_count = lunches.count % PERSONS_PER_GROUP
+    # groups_with_5 = groups.pop(groups_with_5_count)
+    # groups_with_4 = groups
+    groups.each do |group|
+      remaining_lunches = lunches.where(group: nil)
+      group.create_group!(remaining_lunches)
+    end
+  end
 end
--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    count = 1
+    16.times do
+      User.create(email: "#{count}@crowddesk.de")
+      count += 1
+    end
+
+    User.all.each { |u| Lunch.create(user: u, date: Date.today) }
+  end
+
+  test '16 customers get assigned to a group' do
+    Group.create_all_groups!
+
+    group_counts = Group.all.map { |g| g.lunches.count }
+
+    groups_with4 = group_counts.count(4)
+    assert_equal 4, groups_with4
+
+    Group.all.each do |g|
+      assert_equal 4, g.lunches.count
+    end
+
+    users_in_groups = Group.all.map { |g| g.users.map(&:id) }.flatten
+    assert_equal users_in_groups.count, users_in_groups.uniq.count
+    assert_equal Lunch.count, users_in_groups.count
+    assert(Lunch.all.all? { |l| l.group.present? })
+  end
 end
