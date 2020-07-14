6804b14e0ea21f2b10b8f8f66c20bdd399f39673
Kevin Liebholz <liebholz@crowddesk.de>
2020-07-14 15:07:33 +0200

    FIX: #2 routing after lunch#create

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -2,13 +2,13 @@ class LunchesController < ApplicationController
   def index
     @date = Date.parse(params[:date]) rescue Date.today
     @can_register = @current_user.lunches.new(date: @date).valid? && @date.future?
-    @other_lunches = Lunch.on(@date).where.not(user: @current_user)
+    @other_lunches = Lunch.on(@date)
   end
 
   def create
     date = Date.parse(params[:date])
     @current_user.lunches.create(date: date)
 
-    redirect_to lunches_path(date: date)
+    redirect_to root_path(date: date)
   end
 end
