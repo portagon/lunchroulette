44b5549f480505c6bde61936ef790efad493b3fc
Kevin Liebholz <liebholz@crowddesk.de>
2020-07-15 08:58:26 +0200

    CLEAN: #4 refactored group creation

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,30 +9,41 @@ class Group < ApplicationRecord
   validates :date, :size, presence: true
 
   def self.create_all_groups!
+    vars = initialize_groups_and_find_lunches
+    groups = assign_group_sizes(vars[:lunches], vars[:groups])
+    send_groups(vars[:lunches], groups)
+  end
+
+  def self.initialize_groups_and_find_lunches
     date = Date.today
     lunches = Lunch.on(date)
     groups_count = lunches.count / PERSONS_PER_GROUP
     groups_count += 1 if lunches.count % PERSONS_PER_GROUP >= MIN_PERSONS_PER_GROUP
     groups = (1..groups_count).map { Group.new(date: date, size: PERSONS_PER_GROUP) }
-    assign_group_sizes(lunches, groups)
+    { groups: groups, lunches: lunches }
   end
 
   def self.assign_group_sizes(lunches, groups)
     rest_lunches = lunches.count % PERSONS_PER_GROUP
+    return groups if rest_lunches.zero?
 
-    unless rest_lunches == 0
-      if rest_lunches >= MIN_PERSONS_PER_GROUP
-        groups.last.size = rest_lunches # small group
-      elsif rest_lunches < MIN_PERSONS_PER_GROUP && groups.count == 1
-        groups.last.size = lunches.count # one group with all lunches
-      elsif rest_lunches < MIN_PERSONS_PER_GROUP
-        big_groups_count = lunches.count % PERSONS_PER_GROUP
-        count = rest_lunches / big_groups_count + PERSONS_PER_GROUP
-        groups.sample(big_groups_count).each { |group| group.size = count }
-      end
+    if rest_lunches >= MIN_PERSONS_PER_GROUP
+      groups.last.size = rest_lunches # small group
+    elsif rest_lunches < MIN_PERSONS_PER_GROUP && groups.count == 1
+      groups.last.size = lunches.count # one group with all lunches
+    elsif rest_lunches < MIN_PERSONS_PER_GROUP
+      return handle_big_groups(lunches, rest_lunches, groups)
     end
 
-    send_groups(lunches, groups) # full groups
+    groups # all groups incl. full ones
+  end
+
+  def self.handle_big_groups(lunches, rest_lunches, groups)
+    big_groups_count = lunches.count % PERSONS_PER_GROUP
+    count = rest_lunches / big_groups_count + PERSONS_PER_GROUP
+    groups.sample(big_groups_count).each { |group| group.size = count }
+
+    groups
   end
 
   def self.send_groups(lunches, groups)
