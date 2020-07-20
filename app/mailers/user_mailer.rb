fb7efbee333a756cbf41ae41f92883e3368ad884
Kevin Liebholz <liebholz@crowddesk.de>
2020-07-20 07:59:50 +0200

    FIX-WIP: #9 test only sending mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,4 +8,9 @@ class UserMailer < ApplicationMailer
     @lunch = lunch
     mail(to: @lunch.user.email, subject: "Your lunch on #{@lunch.date} was confirmed!")
   end
+
+  def send_kevin
+    @user = User.find_by(email: 'liebholz@crowddesk.de')
+    mail(to: @user.email, subject: 'from send_kevin', body: 'you got it man!')
+  end
 end
