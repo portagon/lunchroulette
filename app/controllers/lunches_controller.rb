68a65c9ef00eb3505cc119b3049e74df52600813
nanookbergmann <inw.bergmann@gmail.com>
2020-09-04 15:27:57 +0200

    CLEAN: automatically select the next occuring thursday in root_path-#12

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,6 +1,6 @@
 class LunchesController < ApplicationController
   def index
-    @date = Date.parse(params[:date]) rescue Date.today
+    @date = Date.parse(params[:date]) rescue Date.today.next_occurring(:thursday)
     @day_ok = day_ok?
     @can_register = @current_user.lunches.new(date: @date).valid? && day_ok?
     @other_lunches = Lunch.on(@date)
